f6d7d86b80795d98c929ca5e1f704b057867c5d6
Konrad Głowacki <konrad@webkrak.pl>
2015-02-14 17:55:40 +0100

    Reorganize

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,31 @@
-exports.index = function(req, res) {
-  res.render('index');
-};
+var express = require('express'),
+    form = require('express-form');
 
-exports.saveUser = function(req, res) {
-  if (req.form.isValid) {
-    res.render('thanks', { email: req.form.email });
-  } else {
-    res.render('index', {
-      name: req.form.name,
-      surname: req.form.surname,
-      email: req.form.email,
-      errors: req.form.errors
-    });
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/',
+  form(
+    form.field('name').trim().required(),
+    form.field('surname').trim().required(),
+    form.field('email').trim().toLower().required().isEmail()
+  ),
+  function(req, res) {
+    if (req.form.isValid) {
+      res.render('thanks', { email: req.form.email });
+    } else {
+      res.render('index', {
+        name: req.form.name,
+        surname: req.form.surname,
+        email: req.form.email,
+        errors: req.form.errors
+      });
+    }
   }
-};
+);
+
+module.exports = router;
