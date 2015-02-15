2a416bad22b92e16535c4f7272609f71916de6ec
Konrad Głowacki <konrad@webkrak.pl>
2015-02-16 00:54:46 +0100

    Mailer

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
 var express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    Mailer = require('../lib/mailer');
 
 router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/test', function(req, res, next) {
+  var data = { name: 'Zenek' };
+
+  var mailer = new Mailer({
+    template: 'test',
+    recipients: 'test@wp.pl',
+    subject: 'Some subject'
+  }, data);
+
+  mailer.send(function(err, result) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+
   res.render('index');
 });
 
