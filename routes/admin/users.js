8bbabaf6677682ff8c38312baf17f48150412923
Konrad Głowacki <konrad@webkrak.pl>
2015-02-15 17:34:17 +0100

    Delete Users

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -2,10 +2,21 @@ var express = require('express');
 var router = express.Router();
 var models = require('../../models');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   models.User.findAll({ include: [models.Group] }).then(function(users) {
-    res.render('users/index', { users: users });
+    res.render('admin/users/index', { users: users });
+  });
+});
+
+router.get('/:id/destroy', function(req, res, next) {
+  models.User.find(req.params.id).then(function(user) {
+    if (user) {
+      user.destroy().then(function() {
+        res.redirect('/admin/users');
+      });
+    } else {
+      res.render('error', { message: 'User not exist!' });
+    }
   });
 });
 
