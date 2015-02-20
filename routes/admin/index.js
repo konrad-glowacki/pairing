aca9da55c98f8e5e11c8411911c56833f7a3fd6c
Konrad Głowacki <konrad@webkrak.pl>
2015-02-20 18:37:29 +0100

    Order User by id in admin panel

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -4,7 +4,7 @@ var express = require('express'),
     models = require('../../models');
 
 router.get('/', function(req, res, next) {
-  models.Group.findAll({ include: [models.User] }).then(function(groups) {
+  models.Group.findAll({ include: [models.User], order: [[models.User, 'id']] }).then(function(groups) {
     res.render('admin/index', { groups: groups, dateFormat: dateFormat });
   });
 });
